b9081d8b74bbd9c9ccc56007b3b06ab02c15bfe5
Minh Nguyet <nguyetminh842@gmail.com>
2018-09-19 20:32:30 +0700

    [layout] report problem

---
+++ app/src/main/java/com/dut2018/fsometeam/savior/problem_report.java
@@ -0,0 +1,13 @@
+package com.dut2018.fsometeam.savior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class problem_report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_report);
+    }
+}
